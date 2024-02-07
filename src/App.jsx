d1bf5f322ef31f9e0e80cd3675970f89aba46169
Dimi Tsonev <dconev@gmail.com>
2024-02-07 19:54:49 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar'
 import Homepage from './components/pages/Homepage'
 import About from './components/pages/About'
 import BookPage from './components/pages/BookPage'
 import Bookshelf from './components/pages/Bookshelf'
 import Footer from './components/Footer'
-
 import Header from './components/Header'
+import Navbar from './components/Navbar';
 
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import  Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <Navbar />
+    </header>
+  );
+};
 
-export default Header
+export default Header;

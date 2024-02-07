ccee1dfb5e99fce2c17a5943bb72184648ddf3f1
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-02-07 17:07:49 +0000

    added basic css structure in index.css using comments, removed app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 import Homepage from './components/pages/Homepage'
 import About from './components/pages/About'

e82bc057060c1945964d1a54b68422a3351ab594
Miriam Cristina Zarzu <145344909+MiriamCristinaZ@users.noreply.github.com>
2024-02-08 21:24:19 +0000

    removed searchbar

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import SearchBar from "../SearchBar.jsx"
-import Header from "../Header.jsx"
-
+import React from "react";
+import Header from "../Header.jsx";
 
 const Homepage = () => {
   return (
     <>
       <Header />
-      <SearchBar />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;

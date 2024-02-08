b548893c36b6aea3106b1056f39909401b95ca48
Edona Ahmetaj <edonamasur@gmail.com>
2024-02-08 20:35:58 +0000

    created header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import  Navbar from './Navbar'
+import  Navbar from './Navbar';
+import headerImg from '../assets/headerphoto.jpeg';
 
 const Header = () => {
   return (
-    <header>
+    <header id="header">
+      <img id="header-image" src= {headerImg} alt="Book" />
+      <h1 id="header-title">BOOK BRIDGE</h1>
+      <h2 id="header-subtitle">Search for your book.</h2>
     </header>
   );
 };
--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import SearchBar from "../SearchBar.jsx"
+import Header from "../Header.jsx"
+
 
 const Homepage = () => {
   return (
     <>
+      <Header />
       <SearchBar />
     </>
   )

31f5a0e466a785e6960fa60190dc149487ca8e5d
Dimi Tsonev <dconev@gmail.com>
2024-02-11 16:32:10 +0000

    passing props in book page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ function App() {
       <Route path='/' element= {<Homepage/>} />
       <Route path='/about' element={<About/>}/> 
       <Route path='/book-page' element={<BookPage/>}/>
+      <Route path='/book-page' element={<BookPage  search={search} books={books} handleChange={handleChange} handleSubmit={handleSubmit} />}/>
+
       <Route path='/book-shelf' element={<Bookshelf/>}/>
       </Routes>
       <Footer />

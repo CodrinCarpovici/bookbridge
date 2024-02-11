0986bb63a5aecc77df77baab16a699520e969059
Dimi Tsonev <dconev@gmail.com>
2024-02-11 16:39:38 +0000

    passing props from App to book card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -8,11 +8,11 @@ const BookCard = ({ coverImg,title, author, releaseYear }) => {
     <div className="card">
       <Link to="/book-page">
         <motion.div whileHover={{ scale: 1.15 }}className="card-img-container">
-          <img src={book} alt="Book Cover" className="card-img" />
+          <img src={coverImg} alt="Book Cover" className="card-img" />
           <div className="card-hover-content">
-            <h5 className="card-title">{title}Harry Potter and the cursed child</h5>
-            <p className="card-text">Author:  by J. K. Rowling, {author}</p>
-            <p className="card-text">Year: 2016 {releaseYear}</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{author}</p>
+            <p className="card-text">{releaseYear}</p>
             <button className="btn btn-dark">Add</button>
           </div>
         </motion.div>

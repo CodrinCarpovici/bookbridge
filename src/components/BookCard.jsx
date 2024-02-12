3b439a9dd0b437a982a2513aa24c64cf3fc8fe8b
Dimi Tsonev <dconev@gmail.com>
2024-02-12 19:26:53 +0000

    bugs

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import book from "../assets/book.jpg";
+// import book from "../assets/book.jpg";
 
 
 const BookCard = ({ books,coverImg,title, author, releaseYear }) => {

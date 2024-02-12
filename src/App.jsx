ce7cf6f6a260d5569bc58b06ee4a97cca631b12e
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-02-12 20:51:42 +0000

    working api call and card generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './components/pages/Homepage'
-import About from './components/pages/About'
-import BookPage from './components/pages/BookPage'
-import Bookshelf from './components/pages/Bookshelf'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar';
-
-
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./components/pages/Homepage";
+import About from "./components/pages/About";
+import BookPage from "./components/pages/BookPage";
+import Bookshelf from "./components/pages/Bookshelf";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [books, setBooks] = useState([]);
 
-  const apiKey = "AIzaSyBaXf-WaFNjuXH_bgd4jUptijqnQjKFmMk"
+  const apiKey = "AIzaSyBaXf-WaFNjuXH_bgd4jUptijqnQjKFmMk";
 
   // useEffect(() => {
   //   const debounceTimer = setTimeout(() => {
@@ -46,40 +42,51 @@ function App() {
   };
 
   const handleSubmit = () => {
-    fetchData()}
+    fetchData();
+  };
 
-    const fetchData = () => {
-      const fetchBooks = async () => {
-        try {
-          const response = await fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${apiKey}`
-          );
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const data = await response.json();
-          setBooks(data.items);
-          console.log(data.items)
-        } catch (error) {
-          console.error('Error fetching data:', error);
+  const fetchData = () => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${apiKey}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
+        const data = await response.json();
+        setBooks(data.items);
+        console.log(data.items);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-      if (search !== '') {
-        fetchBooks();
-      }
+    };
+    if (search !== "") {
+      fetchBooks();
     }
+  };
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path='/' element= {<Homepage search={search} books={books} handleChange={handleChange} handleSubmit={handleSubmit}/>} />
-      <Route path='/about' element={<About/>}/> 
-      <Route path='/book-page' element={<BookPage/>}/>
-      <Route path='/book-shelf' element={<Bookshelf/>}/>
+        <Route
+          path="/"
+          element={
+            <Homepage
+              search={search}
+              books={books}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/book-page" element={<BookPage />} />
+        <Route path="/book-shelf" element={<Bookshelf />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import headerImg from "../assets/headerphoto.jpeg";
 import SearchBar from "./SearchBar";
 import BookCard from "./BookCard";
 
-const Header = ({handleChange, handleSubmit, books, search}) => {
+const Header = ({ handleChange, handleSubmit, books, search }) => {
   return (
     <header
       className="header-background d-flex flex-column justify-content-center align-items-center text-center"
@@ -16,8 +16,27 @@ const Header = ({handleChange, handleSubmit, books, search}) => {
         </h2>
       </div>
       <div className="w-100 d-flex justify-content-center mt-4">
-        <SearchBar search={search} books={books} handleChange={handleChange} handleSubmit={handleSubmit}/>
-        {books.map((book) => (<BookCard  key= {book.id} coverImg ={book.volumeInfo.imageLinks.thumbnail} title={book.title} author={books.authors} releaseYear={books.publishedDate} />) )}
+        <SearchBar
+          search={search}
+          books={books}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+        {books && books.length > 0 ? (
+          books.map((book) => (
+            <BookCard
+              key={book.id}
+              coverImg={book.volumeInfo.imageLinks.thumbnail}
+              title={book.volumeInfo.title}
+              author={
+                book.volumeInfo.authors ? book.volumeInfo.authors[0] : "Unknown"
+              }
+              releaseYear={book.volumeInfo.publishedDate}
+            />
+          ))
+        ) : (
+          <p>No books found</p>
+        )}
       </div>
     </header>
   );

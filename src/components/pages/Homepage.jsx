cb6414c2bd8eaec5949f77ae940cffd2973616fe
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-02-13 19:46:09 +0000

    homepage overflow-x hidden

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -3,10 +3,10 @@ import Header from "../Header.jsx";
 
 const Homepage = ({handleChange, handleSubmit, books, search}) => {
   return (
-    <>
+    <div className="homepage">
       <Header search={search} books={books} handleChange={handleChange} handleSubmit={handleSubmit} />
       
-    </>
+    </div>
   );
 };
 
